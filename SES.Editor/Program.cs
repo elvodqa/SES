a6d640563f650b547faa3ec6c4a46c388cdb7d9e
elvodqa <emircengiz21@gmail.com>
2023-03-28 16:39:00 +0300

    Add loading components from assemblies

--- SES.Editor/Program.cs
+++ SES.Editor/Program.cs
@@ -1,5 +1,49 @@
-﻿using SES.Editor;
+﻿using System.Reflection;
+using SES.ECS;
+using SES.Editor;
 using Silk.NET.Maths;
 using Silk.NET.Windowing;
+
+List<Component> GetAllSESComponents()
+{
+    //Assembly assembly = Assembly.GetExecutingAssembly();
+    Assembly? assembly = Assembly.Load("SES");
+    Console.WriteLine(assembly.FullName);
+
+    Type componentType = typeof(Component);
+    List<Component> components = new List<Component>();
+
+    foreach (Type type in assembly.GetTypes())
+    {
+        if (type.IsSubclassOf(componentType))
+        {
+            Console.WriteLine(type.Name);
+            Component instance = (Component)Activator.CreateInstance(type);
+            components.Add(instance);
+        }
+    }
+    return components;
+}
+
+// TODO: CUSTOM COMPONENTS
+List<Component> GetAllCustomComponents(string userAssemblyName)
+{
+    Assembly? assembly = Assembly.Load(userAssemblyName);
+    Console.WriteLine(assembly.FullName);
+
+    Type componentType = typeof(Component);
+    List<Component> components = new List<Component>();
+
+    foreach (Type type in assembly.GetTypes())
+    {
+        if (type.IsSubclassOf(componentType))
+        {
+            Console.WriteLine(type.Name);
+            Component instance = (Component)Activator.CreateInstance(type);
+            components.Add(instance);
+        }
+    }
+    return components;
+}
 
 Editor editor = new();
